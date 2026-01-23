ddf1d642b22c92d252c5c3c497f25ab94ff5dc8a
Keita Ichii <kichii@cnmc.org>
2026-01-23 16:28:30 -0500

    adjusted python version number

--- scripts/1_collector_pipeline.sh
+++ scripts/1_collector_pipeline.sh
@@ -27,20 +27,20 @@ echo
 
 # run the merge script on each species folders - only if proteomes were downloaded
 if [[ -f "data/1_raw/rv_proteome/rv_ncbi_proteome.faa" && -f "data/1_raw/rv_proteome/rv_uniprot_proteome.fasta" ]]; then
-    python3 src/1_collector/2_cdhit_merge_fastas.py data/1_raw/rv_proteome/rv_ncbi_proteome.faa data/1_raw/rv_proteome/rv_uniprot_proteome.fasta data/2_merged/merged_RV.tsv --identity 1.0
+    python3.11 src/1_collector/2_cdhit_merge_fastas.py data/1_raw/rv_proteome/rv_ncbi_proteome.faa data/1_raw/rv_proteome/rv_uniprot_proteome.fasta data/2_merged/merged_RV.tsv --identity 1.0
 fi
 
 if [[ -f "data/1_raw/he_proteome/he_ncbi_proteome.faa" && -f "data/1_raw/he_proteome/he_uniprot_proteome.fasta" ]]; then
-    python3 src/1_collector/2_cdhit_merge_fastas.py data/1_raw/he_proteome/he_ncbi_proteome.faa data/1_raw/he_proteome/he_uniprot_proteome.fasta data/2_merged/merged_HE.tsv --identity 1.0
+    python3.11 src/1_collector/2_cdhit_merge_fastas.py data/1_raw/he_proteome/he_ncbi_proteome.faa data/1_raw/he_proteome/he_uniprot_proteome.fasta data/2_merged/merged_HE.tsv --identity 1.0
 fi
 echo "Merging of FASTA files completed."
 echo
 
 # run the protein info pull script on each merged file
 if [[ -f "data/2_merged/merged_RV.tsv" ]]; then
-    python3 src/1_collector/3_pull_uniprot_protein_info.py data/2_merged/merged_RV.tsv data/3_organized/protein_info_RV.tsv --method batch
+    python3.11 src/1_collector/3_pull_uniprot_protein_info.py data/2_merged/merged_RV.tsv data/3_organized/protein_info_RV.tsv --method batch
 fi
 
 if [[ -f "data/2_merged/merged_HE.tsv" ]]; then
-    python3 src/1_collector/3_pull_uniprot_protein_info.py data/2_merged/merged_HE.tsv data/3_organized/protein_info_HE.tsv --method batch
+    python3.11 src/1_collector/3_pull_uniprot_protein_info.py data/2_merged/merged_HE.tsv data/3_organized/protein_info_HE.tsv --method batch
 fi
