25fe977653612e3ecfc419c091ec350dc4c5b3c4
Keita Ichii <kichii@cnmc.org>
2026-01-30 09:30:42 -0500

    added STRING PPI download for RV

--- src/1_collector/4_download_string_ppi.sh
+++ src/1_collector/4_download_string_ppi.sh
@@ -3,3 +3,16 @@
 # This script is used to download the STRING PPI data for Ramazzottius varieornatus (missing for Hypsibius exemplaris)
 echo "Downloading STRING PPI data..."
 
+# if none make directory for STRING data
+mkdir -p data/1_raw/string_ppi
+
+# Download STRING PPI data for Ramazzottius varieornatus (taxonomy ID: 947166)
+echo "Downloading Ramazzottius varieornatus PPI data..."
+curl -o data/1_raw/string_ppi/rv_ppi.txt.gz "https://stringdb-downloads.org/download/protein.links.v12.0/947166.protein.links.v12.0.txt.gz"
+gunzip data/1_raw/string_ppi/rv_ppi.txt.gz
+
+# Download STRING PPI data for Hypsibius exemplaris (taxonomy ID: 2072580)
+# WIP: Currently not available on STRING database, so skipping this step
+
+echo "STRING PPI data download completed."
+echo
