c535636a6d91b1ff82e55cc9b5326e48e4758c52
Keita Ichii <kichii@cnmc.org>
2026-02-05 14:17:30 -0500

    Update submit_job.sh

--- submit_job.sh
+++ submit_job.sh
@@ -4,14 +4,19 @@
 #SBATCH --job-name=KG_data_collection_pipeline
 #SBATCH --error=logs/collector_pipeline_%j.err
 #SBATCH --output=logs/collector_pipeline_%j.out
-#SBATCH --time=01:00:00
+#SBATCH --time=2:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem=16G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu  # Use GPU partition
+
+mkdir -p logs
 
 # Initialize micromamba environment
+module load micromamba
 eval "$(micromamba shell hook --shell bash)"
 micromamba activate tardigrade_proteome_KG_env
 
 # Run the collector pipeline script
-bash scripts/1_collector_pipeline.sh
+python3 src/2_embedder/1_generate_embeddings.py data/3_organized/ embeddings_test_hpc.npz --test_mode
