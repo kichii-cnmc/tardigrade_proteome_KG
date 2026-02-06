dad3e23f10285b1715b03db66f6a577719042d90
Keita Ichii <kichii@cnmc.org>
2026-02-06 12:19:40 -0500

    added comments

--- src/2_embedder/2_calculate_clusters.py
+++ src/2_embedder/2_calculate_clusters.py
@@ -1,2 +1,4 @@
 # Script used to take the compressed ESM-2 embeddings and calculate clusters using PCA and K-means.
 
+# PCA is applied, apply normalization, cluster by kmeans cosine similarity, visualize clusters with UMAP, save cluster assignments and centroids for later use in the KG construction step.
+
