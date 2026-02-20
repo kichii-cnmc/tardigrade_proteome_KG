2158c448c4847b4b83ff092d6255ef8d4429edbe
Keita Ichii <kichii@cnmc.org>
2026-02-20 11:50:00 -0500

    fixed compare_embeddings script call

--- scripts/2_embedding_pipeline.sh
+++ scripts/2_embedding_pipeline.sh
@@ -48,7 +48,7 @@ fi
 python3 src/2_embedder/2_pca_reduction.py data/1_raw/embeddings/raw_embeddings.npz data/1_raw/embeddings/reduced_embeddings.npz --variance_threshold 0.90
 
 # run the embedding comparison script to calculate cosine similarity matrix & save edge list for graph building
-python3 src/2_embedder/3_compare_embeddings.py data/1_raw/embeddings/reduced_embeddings.npz data/3_organized $(if [ "$TEST_MODE" = true ]; then echo "--test_mode"; fi)
+python3 src/2_embedder/3_compare_embeddings.py data/1_raw/embeddings/reduced_embeddings.npz data/3_organized/ $(if [ "$TEST_MODE" = true ]; then echo "--test_mode"; fi)
 echo "Embedding comparison completed."
 
 # end of script
