13f85abe0f7d448fd4d7c27f04cd2cb6281e3fc3
Keita Ichii <kichii@cnmc.org>
2026-03-02 16:11:28 -0500

    feat(data-validation): build framework for DataValidator class

--- data_analysis/data_validation.py
+++ data_analysis/data_validation.py
@@ -1,7 +1,6 @@
 # script used to generate a validation report and figures for data in 3_organized
 
 import pandas as pd
-import argparse
 
 '''
 Script Checks:
@@ -22,4 +21,38 @@ Assignment-type Dataset Checks (DeepGO, Pfam, PROSITE):
 - Histogram of number of proteins assigned per function/domain; are there functions/domains that are very common vs. rare?
     - Mean, median, range of number of proteins per function/domain
 '''
+
+def load_protein_info(protein_info_file):
+    '''Loads the protein info dataset and returns a DataFrame.'''
+    df = pd.read_csv(protein_info_file, sep='\t')
+    return df
+
+
+class DataValidator:
+    def __init__(self, filepath, base_id_list, dataset_name):
+        self.filepath = filepath
+        self.dataset_name = dataset_name
+        self.data = load_protein_info(filepath)
+        self.base_id_list = base_id_list # list of valid UniProt IDs to check against for consistency
+        self.report = {}
+
+    def return_name(self):
+        return self.dataset_name
+
+if __name__ == "__main__":
+    # Load base UniProt ID list from the protein info datasets for consistency checks
+    rv_uniprot_id_list = load_protein_info('data/3_organized/RV_UniProt_ID.tsv')['UniProt_ID'].tolist()
+    he_uniprot_id_list = load_protein_info('data/3_organized/HE_UniProt_ID.tsv')['UniProt_ID'].tolist()
+    merged_uniprot_id_list = rv_uniprot_id_list + he_uniprot_id_list
+
+    # Create DataValidator instances for each dataset for each organism
+    # sequence (2)
+    # geneid (2)
+    # af structures (1)
+    # deepgo (BP, MF, CC) (1)
+    # pfam (2)
+    # prosite (2)
+    # string ppi (2)
+    # esm-2 similarity (1)
+
 
